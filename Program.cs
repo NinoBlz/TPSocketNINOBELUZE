52811ec18a4242afec49c5465da01f3bf12be355
Nino Beluze <nino.beluze2@gmail.com>
2023-11-12 18:03:15 +0100

    commit 4

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TPSocket.Properties;
 
-namespace TPSocketNINOBELUZE
+namespace TPSocket
 {
-    internal static class Program
+    internal class Program
     {
-        /// <summary>
-        /// Point d'entrée principal de l'application.
-        /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            // Créez une nouvelle instance de votre formulaire Form1
+            Communication mainForm = new Communication();
+
+            // Appelez Application.Run pour démarrer l'application Windows Forms
+            Application.Run(mainForm);
         }
     }
 }
+
